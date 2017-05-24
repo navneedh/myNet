24aea3b79e52f8be97e848c2e0428f1486090092
navneedh <navneedh@airbears2-10-142-150-182.airbears2.1918.berkeley.edu>
2017-05-24 15:13:54 -0700

    Began forwardProp

--- neuralNetwork/neuralNetwork.py
+++ neuralNetwork/neuralNetwork.py
@@ -5,6 +5,7 @@ class NeuralNetwork:
     def __init__(self, layerSize, inputData):
         self.numLayers = layerSize
         self.layerArray = []
+        self.layerSizes = []
         self.input = inputData
         self.inputFeatureSize = inputData.shape[1]
         self.trials = 0
@@ -15,16 +16,25 @@ class NeuralNetwork:
         initialData = 1
 
     def train(self,data):
-        pass
+        self.layerArray[0] = data[0]
+        self.forwardProp(data[0])
+        #for sample in data:
+            #forwardProp()
+            #backwardProp()
+
+    def forwardProp(self, inputData):
+        oneVal = inputData
+        for layer in self.layerArray[1:]:
+            oneVal = np.dot(layer,oneVal)
+            layer.neurons = oneVal
+            print(layer.neurons)
+
 
     def toString(self):
         for layer in self.layerArray:
-            print(layer.size)
-            print("||")
+            print(layer.neurons)
 
-    def constructNet(inputData, *args, initialize='xavier', classify = 'binary'): #need to figure out how I will implmenet multiclass classification
-
-
+    def constructNet(inputData, *args, initialize='gaussian', classify = 'binary'): #need to figure out how I will implmenet multiclass classification
         args, numLayers = list(args), len(args)
 
         #initialize network
@@ -33,10 +43,14 @@ class NeuralNetwork:
 
         if classify == "binary":
             lastLayer = ly.Layer(1, 'end', True)
+        elif classify == "multiclass":
+            lastLayer = ly.Layer(10000, 'end', True) #100 is a filler number
 
         prevSize, prevLayer = inputLayer.size, inputLayer
         args.insert(0,inputLayer)
         args.append(lastLayer)
+        network.layerArray.insert(0,inputLayer)
+        network.layerSizes.append(inputLayer.size)
 
         for layer in args[1:]:
             try:
@@ -46,10 +60,12 @@ class NeuralNetwork:
                 prevLayer.weights = layer.createMatrix(layer.size, prevSize, initialize)
                 #layer.aMatrix = prevLayer.weights
                 network.weightArray.append(prevLayer.weights)
-                print(prevLayer.weights)
+                network.layerSizes.append(layer.size)
                 prevSize, prevLayer = layer.size, layer
             except:
                 print("Wrong initializations")
+
+
 
 class Computation:
 
