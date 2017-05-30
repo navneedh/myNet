b1d168fbd4b194c9df7427a7a3ea134237243dbd
navneedh <navneedh@Navneedhs-MacBook-Pro.local>
2017-05-29 17:10:01 -0700

    Added hinge loss

--- neuralNetwork/computation.py
+++ neuralNetwork/computation.py
@@ -7,6 +7,7 @@ class Computation:
         self.backPropValue = 0
         self.totalError = 0
 
+#activation functions
 def sigmoid(x):
     return 1/ (1 + math.exp(-x))
 
@@ -25,3 +26,25 @@ def relu(x):
 
 def softmax(x):
     pass
+
+#loss functions
+
+def logistic(actual, expected): #binary classification should be used with sigmoid
+    if actual == 1:
+        return -math.log(expected)
+    elif actual == 0:
+        return -math.log(1-expected)
+
+def hinge(actual, expected): #does not need to use sigmoid function before hinge loss, directly take linear output into hinge loss function
+    return math.max(0,1-expected*actual)ß
+
+def crossEntropy(actual,expected): #multiclass classification should be used with softmax
+    pass
+
+def l1(actual, expected):
+    return math.pow(actual-expected,2)
+
+def l2(actual, expected):
+    return math.abs(actual-expected)
+
+#figure out what loss function to use for relu
