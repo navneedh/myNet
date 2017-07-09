0c4c543f531b67b565f7a8100a6733deddd694b5
navneedh <navneedh@Navneedhs-MacBook-Pro.local>
2017-07-08 18:48:01 -0700

    Refactored kmeans.py for plug ins

--- kmeans.py
+++ kmeans.py
@@ -15,46 +15,60 @@ def average(vals):
     return np.mean(vals, axis=0)
 
 
-#create test data
-data = (make_blobs(100,2,4)[0])
-x, y = zip(*data)
-data = []
-for i in range(len(x)):
-    data.append([[x[i],y[i]],0])
+def train(values=100, dimensions=2, blobNum=4, NUM_CENTROIDS=4, ITERATIONS=1000):
+    global x
+    global y
+    global centroids
+    global seedCentroids
+    data = (make_blobs(values,dimensions,blobNum)[0])
+    x, y = zip(*data)
+    data = []
+    for i in range(len(x)):
+        data.append([[x[i],y[i]],0])
 
-#create bounding box
-minPair = [min(x), min(y)]
-maxPair = [max(x),max(y)]
+    #create bounding box
+    minPair = [min(x), min(y)]
+    maxPair = [max(x),max(y)]
 
-centroids = []
-#initialize random centroids
-for i in range(NUM_CENTROIDS):
-    centroids.append([np.random.randint(minPair[0],maxPair[0]),np.random.randint(minPair[1],maxPair[1])])
+    centroids = []
+    #initialize random centroids
+    for i in range(NUM_CENTROIDS):
+        centroids.append([np.random.randint(minPair[0],maxPair[0]),np.random.randint(minPair[1],maxPair[1])])
 
-print(centroids)
-for _ in range(ITERATIONS):
-    valsToAvg = {(x[0]):[] for x in enumerate(centroids)}
-    for i1, elem in enumerate(data):
-        distance = 10000
-        for i2, c in enumerate(centroids):
-            val = calcDistance(c,elem[0])
-            if val < distance:
-                distance = val
-                data[i1][1] = i2
-        valsToAvg[(data[i1][1])].append(elem[0])
 
-    centroids_copy = []
-    for key in valsToAvg.keys():
-        if len(valsToAvg[key]) == 0:
-            centroids_copy.append(centroids[key])
-        else:
-            # print(average(valsToAvg[key]).tolist())
-            centroids_copy.append(average(valsToAvg[key]).tolist())
+    #store seed centroids
+    seedCentroids = list(centroids)
 
-    centroids = centroids_copy
+    for _ in range(ITERATIONS):
+        valsToAvg = {(x[0]):[] for x in enumerate(centroids)}
+        for i1, elem in enumerate(data):
+            distance = 10000
+            for i2, c in enumerate(centroids):
+                val = calcDistance(c,elem[0])
+                if val < distance:
+                    distance = val
+                    data[i1][1] = i2
+            valsToAvg[(data[i1][1])].append(elem[0])
 
-print(centroids)
-plt.plot(x,y, 'ro')
-x, y = zip(*centroids)
-plt.plot(x, y, 'g^', linewidth=2.0)
-plt.show()
+        centroids_copy = []
+        for key in valsToAvg.keys():
+            if len(valsToAvg[key]) == 0:
+                centroids_copy.append(centroids[key])
+            else:
+                # print(average(valsToAvg[key]).tolist())
+                centroids_copy.append(average(valsToAvg[key]).tolist())
+
+        centroids = centroids_copy
+
+def graph():
+    plt.plot(x,y, 'ro')
+    a, b = zip(*centroids)
+    plt.plot(a, b, 'g^', linewidth=2.0)
+    plt.show()
+
+
+if __name__ == '__main__':
+    train()
+    graph()
+    print("Initial:", seedCentroids)
+    print("Final:", centroids)
---
+++ kmeansNet.py
@@ -0,0 +1,1 @@
+import kmeans as km
