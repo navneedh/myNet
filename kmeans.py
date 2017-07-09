deb15398d75aada1dd43baf88e0d2a59ab8ed8dc
navneedh <navneedh@Navneedhs-MacBook-Pro.local>
2017-07-08 18:27:49 -0700

    fixed initializing centroids to always converge

--- kmeans.py
+++ kmeans.py
@@ -5,7 +5,7 @@ import math
 
 #hyperparameters
 NUM_CENTROIDS = 4
-ITERATIONS = 100
+ITERATIONS = 1000
 
 #helper functions
 def calcDistance(arr1, arr2):
@@ -14,6 +14,7 @@ def calcDistance(arr1, arr2):
 def average(vals):
     return np.mean(vals, axis=0)
 
+
 #create test data
 data = (make_blobs(100,2,4)[0])
 x, y = zip(*data)
@@ -21,11 +22,16 @@ data = []
 for i in range(len(x)):
     data.append([[x[i],y[i]],0])
 
+#create bounding box
+minPair = [min(x), min(y)]
+maxPair = [max(x),max(y)]
+
 centroids = []
 #initialize random centroids
 for i in range(NUM_CENTROIDS):
-    centroids.append([np.random.randint(1,15),np.random.randint(1,15)])
+    centroids.append([np.random.randint(minPair[0],maxPair[0]),np.random.randint(minPair[1],maxPair[1])])
 
+print(centroids)
 for _ in range(ITERATIONS):
     valsToAvg = {(x[0]):[] for x in enumerate(centroids)}
     for i1, elem in enumerate(data):
@@ -37,7 +43,6 @@ for _ in range(ITERATIONS):
                 data[i1][1] = i2
         valsToAvg[(data[i1][1])].append(elem[0])
 
-    print(centroids)
     centroids_copy = []
     for key in valsToAvg.keys():
         if len(valsToAvg[key]) == 0:
@@ -48,7 +53,8 @@ for _ in range(ITERATIONS):
 
     centroids = centroids_copy
 
-    print(centroids)
-
+print(centroids)
 plt.plot(x,y, 'ro')
+x, y = zip(*centroids)
+plt.plot(x, y, 'g^', linewidth=2.0)
 plt.show()
