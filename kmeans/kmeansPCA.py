ef09f3c4a4abd5392b40efaa755df055eb8d0043
navneedh <navneedh@gmail.com>
2017-07-13 16:54:09 -0700

    Really low test error, need to use tensorboard

--- kmeans/kmeansPCA.py
+++ kmeans/kmeansPCA.py
@@ -2,8 +2,13 @@ import kmeans as km
 import numpy as np
 import tensorflow as tf
 
+#run a standard data set through this neural network to see if learning happens
+#get summary of data to tensorboard
+#cache training and test data to csv files
+
+
 #hyperparameters
-TRAININGSIZE = 1000
+TRAININGSIZE = 100
 BATCH = TRAININGSIZE//40
 EPOCHS = 100
 display_step = 1
@@ -46,6 +51,10 @@ def bias(dimension):
 weights = {'W1':weights([100,60]), 'W2':weights([60,40]), 'W3': weights([40,15])}
 biases = {'B1': bias(60), 'B2': bias(40), 'B3': bias(15)}
 
+init = tf.global_variables_initializer()
+
+sess.run(init)
+
 def neuralNet():
     x_d = tf.nn.dropout(x,0.8) #might need to fix these hyperparameters
     l1 = tf.nn.relu(tf.matmul(x_d,weights['W1']) + biases['B1'])
@@ -61,24 +70,32 @@ result = neuralNet()
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=result, labels=y_))
 optimizer = tf.train.AdamOptimizer(0.001, 0.9).minimize(cost)
-init = tf.global_variables_initializer()
+correct_prediction = tf.argmax(result,1)
 
-with tf.Session() as sess:
-    sess.run(init)
-    avg_cost = 0
-    for epoch in range(EPOCHS):
-        for i in range(BATCH):
-            print(i)
-            batch_x = ((x_training[i*10:(i+4)*10]))
-            batch_y = (y_training_onehot[i*10:(i+4)*10])
-            # Run optimization op (backprop) and cost op (to get loss value)
-            c = sess.run(cost, feed_dict={x: batch_x, y_: batch_y})
-            # Compute average loss
-            avg_cost += c / BATCH
-        if epoch % display_step == 0:
-            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c))
-    print("Training Complete")
+avg_cost = 0
+for epoch in range(EPOCHS):
+    for i in range(BATCH):
+        print(i)
+        batch_x = ((x_training[i*10:(i+4)*10]))
+        batch_y = (y_training_onehot[i*10:(i+4)*10])
+        # Run optimization op (backprop) and cost op (to get loss value)
+        c = sess.run(cost, feed_dict={x: batch_x, y_: batch_y})
+        # Compute average loss
+        avg_cost += c / BATCH
+    if epoch % display_step == 0:
+        print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c))
+print("Training Complete")
 
-    print("Execute Test")
-    testX = getXVector(100,2,5).T
-    print(sess.run(result, feed_dict={x: testX}))
+
+print("Execute Test")
+totalCorrect = 0
+for _ in range(100):
+    number = np.random.randint(2,15)
+    testX = getXVector(100,2,number).T
+    testX = np.reshape(testX, (1,100))
+    prediction = (sess.run(correct_prediction, feed_dict={x: testX}))
+    print("Correct:", number)
+    if number == prediction:
+        totalCorrect += 1
+
+print("Testing Accuracy:", totalCorrect/100)
