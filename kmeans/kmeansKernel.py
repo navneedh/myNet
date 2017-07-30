3d618df479c02694bfeffc3d581a588303cf5700
navneedh <navneedh@gmail.com>
2017-07-30 09:54:06 -0800

    finished pulling kernel data w/out PCA

--- kmeans/kmeansKernel.py
+++ kmeans/kmeansKernel.py
@@ -1,13 +1,14 @@
 import tensorflow as tf
 import pandas as pd
 import kmeans as km
+import numpy as np
 import math
 
 #create kernel matrix
 
 #hyperparameters
 TRAININGSIZE = 500
-BATCH = TRAININGSIZE//50
+BATCH = 20
 EPOCHS = 1000
 display_step = 1
 
@@ -21,19 +22,23 @@ def rbfkernel(x,y):
     return
 
 def kernel1(x,y):
-    return np.norm(x-y)
+    return np.linalg.norm(x-y)
 
+def genkernel(vec, veclist, kernel = kernel1):
+    return [kernel(vec,np.array(vec1)) for vec1 in veclist]
 
 #generate random y training cluster number values
 y_training = [np.random.randint(1,5) for x in range(TRAININGSIZE)]
 
 def getXVector(points, dimension, clusters):
     x,y = km.trainingData(points,dimension,clusters)
+    xy = list(zip(x,y))
+    print(xy)
+    results = np.array([np.array(genkernel(np.array(xy[i]), xy)) for i in range(len(xy))])
+    print(results.shape)
 
-    #is PCA even useful for a two dimensional matrix
-    matrix = np.vstack((x,y)).T
-    U, S, V = np.linalg.svd(matrix, full_matrices=True)
-
+    U, S, V = np.linalg.svd(results, full_matrices=True)
+    print(S)
     # return first principal component vector
     x_training = U[:,0]
 
